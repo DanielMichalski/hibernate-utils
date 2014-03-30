f5eabdaede067684f6c9a2ba52344bce37f85d39
bakeraw2 <notarget1>
2014-03-30 18:24:55 +0200

    Poprawki

--- R1 - Saving and retriving objects/src/main/java/Main.java
+++ R1 - Saving and retriving objects/src/main/java/Main.java
@@ -3,25 +3,32 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.util.Date;
 
 /**
  * Author: Daniel
  */
 public class Main {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.configure();
-        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        Configuration configuration = new Configuration().configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
 
         UserDetails userDetails = new UserDetails();
         userDetails.setUserName("Thomas");
+        userDetails.setAddress("User address");
+        userDetails.setJoindedDate(new Date());
+        userDetails.setDescription("descrption");
 
         Transaction transaction = session.beginTransaction();
         session.persist(userDetails);
         transaction.commit();
+        session.close();
+
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        userDetails = (UserDetails) session.get(UserDetails.class, 1);
+        System.out.println(userDetails);
     }
 }
